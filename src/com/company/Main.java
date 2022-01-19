c01d35d86400e28b112d7ef041072de1bf3f4f79
BhavinThakkar18 <bhavin.majethia18@gmail.com>
2022-01-19 14:46:32 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static void printBinary(int n)
+    {
+        if(n<=0)
+        {
+            System.out.println();
+            return;
+        }
+        Queue<Integer> queue= new LinkedList<>();
+        queue.add(1);
+        for(int i=0;i<n;i++)
+        {
+            int current =queue.remove();
+            System.out.println(current);
+            queue.add(current*10);
+            queue.add(current*10+1);
+
+        }
+    }
+    public static void main(String[] args) {
+    printBinary(3);
+    printBinary(0);
+    printBinary(-5);
+    printBinary(7);
+    printBinary(9);
+    }
+}
